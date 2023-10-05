47294f0aba6b2f70027ea6ef4cc16a3cca5bc3c7
Ashley Likes <alikes1987@gmail.com>
2023-10-05 13:59:52 -0500

    Commit

--- 4.3.1.7 How Many Days.py
+++ 4.3.1.7 How Many Days.py
@@ -4,6 +4,7 @@
 
 # The initial part of the function is ready. Now, convince the function to return None if its arguments don't make sense
 
+## code from 4.2.1.6 that tests for Leap Year
 def isLeapYear(year):
 	if year % 4 == 0:
 		return True
@@ -15,20 +16,29 @@ def isLeapYear(year):
 		return False
 
 def daysInMonth(year, month):
+	# creating a list that contains the number of days in each month
+    # lists are indexed at zero. 
 	monthLengths = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+	# Check if the year and month are valid
 	if month < 1 or month > 12 or year < 1:
+		# if it's not valid, it will return None
 		return None
+	#Accounting for a Leap year using our isLeapYear function
 	if isLeapYear(year):
+		# if it is a leap year, we will modify our list at index 1 and change the number
+        # of days to 29
+        # remember, our list is indexed at zero 
 		monthLengths[1] = 29
-		#lists are indexed at zero
-        # months are indexed at 1
-        # ex: March is the third month, but it's index is 2
+		# if it is not leap year, return the number of days for that specific month
+        # because our list is indexed at zero but our months are indexed at 1, we need to 
+        # subtract one from each month to access the correct index
+        # Ex: March is the 3rd month, but the monthLength for March is index 2 | (3 - 1 = 2)
 		return monthLengths[month - 1]
 
 
 	
 
-
+## Test code provided 
 test_years = [1900, 2000, 2016, 1987]
 test_months = [2, 2, 1, 11]
 test_results = [28, 29, 31, 30]
